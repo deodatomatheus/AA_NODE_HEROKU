e36e2a67965785c91944bafebb7491409a24e7a1
Matheus Deodato <mdo2@cin.ufpe.br>
2021-11-10 01:48:15 -0300

    Adcionando index.html

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+app.use(express.static('public'))
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
-    res.send("Teste do deploy OK")
+    //res.send("Teste do deploy OK")
+    res.render('index')
 })
 
 app.listen(port)
